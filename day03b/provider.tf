3976e231647369aea4aa2e9e4009813c985cf53f
chukmunnlee <chukmunnlee@gmail.com>
2022-06-30 09:14:46 +0800

    FIXED: provider typo in workshop02

--- day03b/provider.tf
+++ day03b/provider.tf
@@ -9,16 +9,7 @@ terraform {
       source = "hashicorp/local"
       version = "2.2.3"
     }
-    cloudflare = {
-      source = "cloudflare/cloudflare"
-      version = "3.18.0"
-    }
   }
-}
-
-provider cloudflare {
-  email = var.CF_email
-  api_token = var.CF_api_token
 }
 
 provider digitalocean {
